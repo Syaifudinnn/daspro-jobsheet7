a749c2f2e6737db6e75616ca0f27811214f6ab62
Syaifudinnn <mukhammad_adn_30rpl@student.smktelkom-mlg.sch.id>
2024-10-10 16:46:23 +0700

    final

--- Tugas2.java
+++ Tugas2.java
@@ -28,12 +28,8 @@ public class Tugas2 {
             System.out.print("Masukkan jenis kendaraan\t : ");
             jenis = input.nextInt();
 
-            //batal jika input == 0
-            if (jenis == 0) {
-                System.out.println("----------------------------------------");
-                System.out.println("Program Selesai");
-                break;
-            }
+            //total keseluruhan
+            totalAll += total;
 
             //jika lanjut
             if (jenis == 1 || jenis == 2) {
@@ -46,23 +42,22 @@ public class Tugas2 {
                     total = 12500;
                     System.out.println("total bayar\t\t\t : " + total);
                 }
-                if (jenis == 1) {
-                    total = durasi * 3000;
-                    System.out.println("total bayar\t\t\t : " + total);
+                else {
+                    if (jenis == 1) {
+                        total = durasi * 3000;    
+                        System.out.println("total bayar\t\t\t : " + total);                    
+                    }
+                    else if (jenis == 2) {
+                        total = durasi * 2000;
+                        System.out.println("total bayar\t\t\t : " + total);
+                    }
                 }
-                else if (jenis == 2) {
-                    total = durasi * 2000;
-                    System.out.println("total bayar\t\t\t : " + total);
-                }
-                
             }
-
-            //total keseluruhan
-            totalAll += total;
 
         } while (jenis != 0);
 
         //output ketika selesai
+        System.out.println("----------------------------------------");
         System.out.println("Total pembayaran parkir\t\t : " + totalAll);
 
     }
