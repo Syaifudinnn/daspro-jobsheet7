9c4304aa7eebb413c1f5446c03ffb06dc01b6bc1
Syaifudinnn <mukhammad_adn_30rpl@student.smktelkom-mlg.sch.id>
2024-10-10 16:27:15 +0700

    p

---
+++ Tugas1.java
@@ -0,0 +1,75 @@
+import java.util.Scanner;
+
+public class Tugas1 {
+    
+    public static void main(String[] args) {
+        
+        //deklarasi inputan
+        Scanner input = new Scanner(System.in);
+
+        //deklarasi variable
+        int hargaTiket = 50000;
+        int jml_tiket, total_tiket_terjual = 0;
+        float diskon = 0, total_harga = 0, harga_diskon = 0, total_penjualan = 0;
+        boolean isLanjut = true;
+
+        //judul
+        System.out.println("=========================================");
+        System.out.println("======= Perhitungan Tiket Bioskop =======");
+        System.out.println("=========================================");
+
+        //logic dan inputan
+        while (isLanjut) {
+
+            //input jumlah tiket yang akan dibeli
+            System.out.print("Masukkan jumlah tiket (masukkan 0 untuk batal) : ");
+            jml_tiket = input.nextInt();
+
+            //jika batal
+            if (jml_tiket == 0) {
+                System.out.println("Transaksi Selesai");
+                break;
+            }
+
+            //jika input negarif
+            if (jml_tiket < 0) {
+                System.out.println("Jumlah tidak bisa negatif, masukkan jumlah yg benar!");
+                continue;
+            }
+
+            //jika input valid
+            //menghitung jumlah tiket yg terjual
+            total_tiket_terjual += jml_tiket;
+
+            //hitung total harga (bayar)
+            total_harga = jml_tiket * hargaTiket;
+
+            //hitung harga (bayar) setelah diskon
+            if (jml_tiket > 10) {
+                diskon = 0.15f;
+                harga_diskon = total_harga - (total_harga * diskon);
+            }
+            else if (jml_tiket > 4) {
+                diskon = 0.1f;
+                harga_diskon = total_harga - (total_harga * diskon);
+            }
+            else {
+                harga_diskon = total_harga;
+            }
+
+            //total penjualan
+            total_penjualan += harga_diskon;
+
+            //output untuk transaksi sementara
+            System.out.println("Total harga untuk " + jml_tiket + " tiket adalah Rp " + harga_diskon);
+
+        };
+
+        //output data semua transaksi
+        System.out.println("=========================================");
+        System.out.println("Total tiket terjual\t : " + total_tiket_terjual);
+        System.out.println("Total penjualan\t\t : " + total_penjualan);
+
+    }
+
+}
---
+++ Tugas2.java
@@ -0,0 +1,76 @@
+import java.util.Scanner;
+
+public class Tugas2 {
+    
+    public static void main(String[] args) {
+        
+        //deklarasi inputan
+        Scanner input = new Scanner(System.in);
+
+        //deklarasi variable
+        int jenis, durasi, total = 0, totalAll = 0;
+
+        //judul
+        System.out.println("========================================");
+        System.out.println("========== Perhitungan Parkir ==========");
+        System.out.println("========================================");
+
+        //daftar kendaraan
+        System.out.println("Daftar kendaraan : ");
+        System.out.println("1. Mobil");
+        System.out.println("2. Motor");
+        System.out.println("0. Keluar");
+
+        //logic & input
+        do {
+            //input kendaraan 
+            System.out.println("----------------------------------------");
+            System.out.print("Masukkan jenis kendaraan\t : ");
+            jenis = input.nextInt();
+
+            //batal jika input == 0
+            if (jenis == 0) {
+                System.out.println("----------------------------------------");
+                System.out.println("Program Selesai");
+                break;
+            }
+
+            //jika input tidak sesuai
+            if (jenis != 0 || jenis != 1 || jenis != 2) {
+                System.out.println("Masukkan kode jenis yg benar!");
+                continue;
+            }
+
+            //jika lanjut
+            if (jenis == 1 || jenis == 2) {
+                //input durasi parkir
+                System.out.print("Masukkan durasi parkir (jam)\t : ");
+                durasi = input.nextInt();
+
+                //ketika durasi lebih dari 5 jam
+                if (durasi > 5) {
+                    total = 12500;
+                    System.out.println("total bayar\t\t\t : " + total);
+                }
+                if (jenis == 1) {
+                    total = durasi * 3000;
+                    System.out.println("total bayar\t\t\t : " + total);
+                }
+                else if (jenis == 2) {
+                    total = durasi * 2000;
+                    System.out.println("total bayar\t\t\t : " + total);
+                }
+                
+            }
+
+            //total keseluruhan
+            totalAll += total;
+
+        } while (jenis != 0);
+
+        //output ketika selesai
+        System.out.println("Total pembayaran parkir\t\t : " + totalAll);
+
+    }
+
+}
