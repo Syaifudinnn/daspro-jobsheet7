648b46970c06deb0b23887e51b4057d8832bdf4c
Syaifudinnn <mukhammad_adn_30rpl@student.smktelkom-mlg.sch.id>
2024-10-10 13:03:08 +0700

    soal no 4

--- SiakadFor16.java
+++ SiakadFor16.java
@@ -9,6 +9,8 @@ public class SiakadFor16 {
 
         //deklarasi variable
         double nilai, tertinggi = 0, terendah = 100;
+        int lulus = 0, tidak_lulus = 0;
+        int batas_lulus = 60;
 
         //judul
         System.out.println("=============================");
@@ -17,9 +19,10 @@ public class SiakadFor16 {
 
         //logic dan inputan
         for (int i = 1; i <= 10; i++) {
-            System.out.print("Masukkan nilai mahasiswa ke-" + i + " : ");
+            System.out.print("Masukkan nilai mahasiswa ke-" + i + "\t: ");
             nilai = input.nextDouble();
             
+            //cari nilai tertinggi dan terendah
             if (nilai > tertinggi) {
                 tertinggi = nilai;
             }
@@ -27,11 +30,21 @@ public class SiakadFor16 {
                 terendah = nilai;
             }
 
+            //cari siswa lulus dan tidak
+            if (nilai >= 60) {
+                lulus++; 
+            }
+            else {
+                tidak_lulus++;
+            }
+
         }
 
         //output
-        System.out.println("Nilai tertinggi : " + tertinggi);
-        System.out.println("Nilai terendah : " + terendah);
+        System.out.println("Nilai tertinggi\t\t\t: " + tertinggi);
+        System.out.println("Nilai terendah\t\t\t: " + terendah);
+        System.out.println("Jumlah mahasiswa lulus\t\t: " + lulus);
+        System.out.println("Jumlah mahasiswa tidak lulus\t: " + tidak_lulus);
 
     }
 
